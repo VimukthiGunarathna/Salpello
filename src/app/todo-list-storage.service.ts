e1a96864a2ed3227bab27bb77ff99d9fc173551f
vimma <vimukthi.2016374@iit.ac.lk>
2020-10-02 11:49:04 +0530

    adding code comments

--- src/app/todo-list-storage.service.ts
+++ src/app/todo-list-storage.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { default_inProgressList, default_todoList, default_doneList } from './todo/data/seed.data';
+// Deault data import
+import { default_inProgressList, default_todoList, default_doneList, default_deletedItems } from './todo/data/seed.data';
 
+// Local storage buckets
 const todoStorage = 'todo';
 const doneStorage = 'done';
 const progressStorage = 'progress';
@@ -14,16 +16,15 @@ export class TodoListStorageService {
   private todoList;
   private doneList;
   private inProgressList;
-
-
-  filePath = '../assets/default-data.json';
+  private deletedItems;
 
   constructor(private httpClient: HttpClient) {
     this.todoList = JSON.parse(localStorage.getItem(todoStorage)) || default_todoList;
     this.doneList = JSON.parse(localStorage.getItem(doneStorage)) || default_doneList;
     this.inProgressList = JSON.parse(localStorage.getItem(progressStorage)) || default_inProgressList;
+    this.deletedItems = JSON.parse(localStorage.getItem(progressStorage)) || default_deletedItems;
   }
-  // get calls
+  // GET calls
   getToDoList() {
     if (this.todoList != null) {
       return [...this.todoList];
@@ -39,20 +40,25 @@ export class TodoListStorageService {
       return [...this.inProgressList];
     }
   }
+  getDeletedItems() {
+    if (this.deletedItems != null) {
+      return [...this.deletedItems];
+    }
+  }
 
-  // post calls
+  // POST calls
   post(item) {
     this.todoList.push(item);
     return this.updateStorage();
   }
 
-  // update calls
+  // UPDATE calls
   put(item, changes) {
     Object.assign(this.todoList[this.findItemIndex(item)], changes);
     return this.updateStorage();
   }
 
-  // delete calls
+  // DELETE calls
   delete(item) {
     this.todoList.splice(this.findItemIndex(item), 1);
     return this.updateStorage();
@@ -64,16 +70,24 @@ export class TodoListStorageService {
   }
 
 
-  // Helper methods
+  // HELPER METHODS
   private updateStorage() {
     localStorage.setItem(todoStorage, JSON.stringify(this.todoList));
     return this.getToDoList();
   }
 
+  /**
+   * Returns 0 if the item exist
+   * @param item : search item
+   */
   private findItemIndex(item) {
     return this.todoList.indexOf(item);
   }
 
+  /**
+   * Returns 0 if the item exist
+   * @param title : search item
+   */
   private findItemIndexofTitle(title) {
     return this.todoList.map(function (e) { return e.title; }).indexOf(title);
   }
